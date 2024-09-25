5a8b71531262d28b1bfa0c45c0267370fd0d9640
LuoXiaobin <412332675@qq.com>
2024-09-25 21:39:31 +0800

    系统布局优化

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 左侧菜单栏的折叠状态
+    collapsed: false,
   },
   getters: {
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;;
+    }
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    }
   },
   modules: {
   },
