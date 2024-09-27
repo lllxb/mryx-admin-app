cc72d85a11fd8f93e28c15fe810efd878900f144
LuoXiaobin <412332675@qq.com>
2024-09-28 03:11:11 +0800

    商品列表页之搜索组件

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
+import store from '@/store';
 const instance = axios.create({
     baseURL: 'https://mallapi.duyiedu.com/',
 })
 
 instance.interceptors.request.use((config) => {
     console.log("config", config);
-    return config;
+    if (config.url.includes('/passport')) {
+        return config;
+    } else {
+        return {
+            ...config,
+            params: {
+                ...config.params,
+                appkey: store.state.user.appkey,
+            }
+        }
+    }
 }, (error) => {
     Promise.reject(error);
 })
--- src/views/page/productList.vue
+++ src/views/page/productList.vue
@@ -1,3 +1,18 @@
 <template>
-    <div>商品列表</div>
+    <div class="product-list">
+        <Search @submit="searchSubmit"></Search>
+    </div>
 </template>
+<script>
+import Search from "@/components/search.vue";
+export default {
+    components: {
+        Search,
+    },
+    methods: {
+        searchSubmit(form) {
+            console.log("searchRes", form);
+        }
+    }
+}
+</script>
